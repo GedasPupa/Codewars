4b95ae4e31b2ce1c12b697c36a78ac7982d16501
GedasPupa <gedosas@gmail.com>
2021-08-31 20:25:02 +0300

    Product Sum Numbers 50:50

--- 4kyu/Product-Sum-Numbers.js
+++ 4kyu/Product-Sum-Numbers.js
@@ -17,6 +17,52 @@
 // Your task is to write an algorithm to compute the sum of all minimal product-sum numbers where 2 ≤ k ≤ n.
 // Courtesy of ProjectEuler.net
 
+const n = 6;
+
 function productSum(n){
-    //..
+    let sum = 0;
+    while (n >= 2) {
+        sum += productSumMY(n--);
+    }
+    return sum;
 }
+
+function productSumMY(n){
+    if (n === 5) {
+        return 0;
+    }
+    const arr1 = [];
+    arr1.length = n;
+    for (let i=0; i<arr1.length; i++) {
+        arr1[i] = 1;
+    }
+    
+    let sumArr = arr1.reduce((red, acc) => red + acc);
+    let multArr = arr1.reduce((red, acc) => red * acc);
+    let nn = arr1.length-1;
+    while (sumArr !== multArr) {            
+        arr1[nn-1] = 2;
+        arr1[nn] += 1;
+        sumArr = arr1.reduce((red, acc) => red + acc);
+        multArr = arr1.reduce((red, acc) => red * acc);
+    }
+    return multArr;
+}
+
+console.log(productSum(n));
+
+
+        // while (sumArr !== multArr) {            
+        //     if (nn >= 0) {
+        //         if (arr1[0] === 9 ) {
+        //             break;
+        //         }
+        //         arr1[nn] += 1;
+        //         nn--;
+        //         sumArr = arr1.reduce((red, acc) => red + acc);
+        //         multArr = arr1.reduce((red, acc) => red * acc);
+        //         console.log('worked');
+        //     } else {
+        //         nn = arr1.length - 1;
+        //     }
+        // }
